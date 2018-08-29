3b71cb2ab3d41c4f018487ea6335e881d0d73f3d
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-29 01:13:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+if array.size==1 
+  return array.join()
+elsif array.size==2 
+  return "#{array[0]} and #{array[1]}"
+else 
+arr1=array.pop()
+arr2=array.join(", ")
+arr3=", and "+arr1
+return arr2+arr3
+end
 end
